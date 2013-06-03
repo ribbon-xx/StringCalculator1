7bb363bc09e8d04159c11f1e8c9aab97fe97a1b1
ribbon-xx <mr.ribbon@gmail.com>
2013-06-03 14:23:58 +0700

    Clean

--- src/com/qsoft/longdt/Calculator.java
+++ src/com/qsoft/longdt/Calculator.java
@@ -13,24 +13,36 @@ public class Calculator {
 	public static int add(String input) {
 		if (input.isEmpty()) {
 			return 0;
-		} else if (input.contains(",")) {
+		} else {
 			String[] tokens = tokenize(input);
 			List<Integer> numbers = convert(tokens, toInt());
 			return sum(numbers).intValue();
-		} else {
-			return Integer.parseInt(input);
 		}
 	}
 
 	private static String[] tokenize(String input) {
-		if (input.startsWith("//")) {
-			Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
-			String customDelimiter = m.group(1);
-			String numbers = m.group(2);
-			return numbers.split(customDelimiter);
+		if (useCustomDelimiter(input)) {
+			return splitUsingCustomDelimiter(input);
+		} else {
+			return splitUsingNewLineAndComma(input);
 		}
+	}
+
+	private static boolean useCustomDelimiter(String input) {
+		return input.startsWith("//");
+	}
+
+	private static String[] splitUsingNewLineAndComma(String input) {
 		String[] tokens = input.split(",|\n");
 		return tokens;
+	}
+
+	private static String[] splitUsingCustomDelimiter(String input) {
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+		m.matches();
+		String customDelimiter = m.group(1);
+		String numbers = m.group(2);
+		return numbers.split(customDelimiter);
 	}
 
 	private static Converter<String, Integer> toInt() {
@@ -46,5 +58,9 @@ public class Calculator {
 
 	private static int toInt(String input) {
 		return Integer.parseInt(String.valueOf(input));
+	}
+
+	public static void main(String[] args) {
+		add("//;\n1;2");
 	}
 }
