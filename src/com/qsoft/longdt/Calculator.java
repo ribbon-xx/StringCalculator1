01bd7fb7bf4ab7aea2e95f753ab9929d166d1ad5
ribbon-xx <mr.ribbon@gmail.com>
2013-06-03 14:00:41 +0700

    Green

--- src/com/qsoft/longdt/Calculator.java
+++ src/com/qsoft/longdt/Calculator.java
@@ -1,14 +1,33 @@
 package com.qsoft.longdt;
 
+import static ch.lambdaj.Lambda.*;
+
+import java.util.List;
+
+import ch.lambdaj.function.convert.Converter;
+
 public class Calculator {
 	public static int add(String input) {
 		if (input.isEmpty()) {
 			return 0;
 		} else if (input.contains(",")) {
 			String[] tokens = input.split(",");
-			return Integer.parseInt(tokens[0]) + Integer.parseInt(tokens[1]);
+			List<Integer> numbers = convert(tokens,
+					new Converter<String, Integer>() {
+
+						@Override
+						public Integer convert(String arg0) {
+							// TODO Auto-generated method stub
+							return toInt(arg0);
+						}
+					});
+			return sum(numbers).intValue();
 		} else {
 			return Integer.parseInt(input);
 		}
+	}
+
+	private static int toInt(String input) {
+		return Integer.parseInt(String.valueOf(input));
 	}
 }
