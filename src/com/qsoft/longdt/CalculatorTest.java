660c264fc8ae7878e6d5eb446fe92e229ef90a0d
ribbon-xx <mr.ribbon@gmail.com>
2013-06-03 13:49:25 +0700

    Red

--- src/com/qsoft/longdt/CalculatorTest.java
+++ src/com/qsoft/longdt/CalculatorTest.java
@@ -7,8 +7,14 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	@Test
-	public void ShouldReturnEmptyOnEmptyString()
+	public void shouldReturnEmptyOnEmptyString()
 	{
 		assertEquals(0, Calculator.add(""));
+	}
+	
+	@Test
+	public void shouldReturnNumberOnNumber()
+	{
+		assertEquals(1, Calculator.add("1"));
 	}
 }
