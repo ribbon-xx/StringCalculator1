00f9762226ded1ac2c848c936719e284c7f92c6b
ribbon-xx <mr.ribbon@gmail.com>
2013-06-03 13:52:54 +0700

    Green

--- src/com/qsoft/longdt/Calculator.java
+++ src/com/qsoft/longdt/Calculator.java
@@ -4,6 +4,9 @@ public class Calculator {
 	public static int add(String input) {
 		if (input.isEmpty()) {
 			return 0;
+		} else if (input.contains(",")) {
+			String[] tokens = input.split(",");
+			return Integer.parseInt(tokens[0]) + Integer.parseInt(tokens[1]);
 		} else {
 			return Integer.parseInt(input);
 		}
